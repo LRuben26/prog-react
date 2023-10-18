66a771370ae8dee1c2ac187fe4f096ddc07f7a57
LRuben26 <luisrubenflorescopa5@gmail.com>
2023-10-18 09:29:11 -0400

    Creacion de mi primer componente

---
+++ pagina1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+        Comida  <code>para </code> mi.-.-.-.-.-.-.
+        </p>
+        <a
+          className="App-link"
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi primera app react
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
